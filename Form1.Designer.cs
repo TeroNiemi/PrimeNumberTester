79934628002b9d453f89500a519569645a479ce2
TeroNiemi <70256874+TeroNiemi@users.noreply.github.com>
2024-09-03 17:42:10 +0300

    prime it is, or not

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace AlkulukuTesti2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tutkittavaluku = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.testaustarkkuus = new System.Windows.Forms.ComboBox();
+            this.tarkista = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tulosFermat = new System.Windows.Forms.Label();
+            this.tulosSolovay = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Alkulukutesti";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fermatin ja Solovay-Strassen algoritmeilla";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(483, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "(C) Tero Niemi 2012 ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(456, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Algoritmit 2 -kurssin harjoitustyö";
+            // 
+            // tutkittavaluku
+            // 
+            this.tutkittavaluku.Location = new System.Drawing.Point(146, 90);
+            this.tutkittavaluku.Name = "tutkittavaluku";
+            this.tutkittavaluku.Size = new System.Drawing.Size(441, 20);
+            this.tutkittavaluku.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Syötä tutkittava luku";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Valitse testaustarkkuus";
+            // 
+            // testaustarkkuus
+            // 
+            this.testaustarkkuus.FormattingEnabled = true;
+            this.testaustarkkuus.Items.AddRange(new object[] {
+            "a = 3, heikko",
+            "a = 100, kohtalaisen tarkka",
+            "a = 1000, hyvin tarkka",
+            "a = 10000, äärimmäisen tarkka"});
+            this.testaustarkkuus.Location = new System.Drawing.Point(146, 122);
+            this.testaustarkkuus.Name = "testaustarkkuus";
+            this.testaustarkkuus.Size = new System.Drawing.Size(257, 21);
+            this.testaustarkkuus.TabIndex = 7;
+            // 
+            // tarkista
+            // 
+            this.tarkista.Location = new System.Drawing.Point(18, 167);
+            this.tarkista.Name = "tarkista";
+            this.tarkista.Size = new System.Drawing.Size(75, 23);
+            this.tarkista.TabIndex = 8;
+            this.tarkista.Text = "Tarkista";
+            this.tarkista.UseVisualStyleBackColor = true;
+            this.tarkista.Click += new System.EventHandler(this.tarkista_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(193, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Fermat";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(143, 299);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Solovay-Strassen";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(621, 361);
+            this.shapeContainer1.TabIndex = 13;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.Location = new System.Drawing.Point(126, 252);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(333, 75);
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 20;
+            this.lineShape1.X2 = 582;
+            this.lineShape1.Y1 = 208;
+            this.lineShape1.Y2 = 208;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(15, 225);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 25);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Tulokset";
+            // 
+            // tulosFermat
+            // 
+            this.tulosFermat.AutoSize = true;
+            this.tulosFermat.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tulosFermat.Location = new System.Drawing.Point(267, 258);
+            this.tulosFermat.Name = "tulosFermat";
+            this.tulosFermat.Size = new System.Drawing.Size(158, 24);
+            this.tulosFermat.TabIndex = 15;
+            this.tulosFermat.Text = "Ei analysoitu vielä";
+            // 
+            // tulosSolovay
+            // 
+            this.tulosSolovay.AutoSize = true;
+            this.tulosSolovay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tulosSolovay.Location = new System.Drawing.Point(267, 291);
+            this.tulosSolovay.Name = "tulosSolovay";
+            this.tulosSolovay.Size = new System.Drawing.Size(158, 24);
+            this.tulosSolovay.TabIndex = 16;
+            this.tulosSolovay.Text = "Ei analysoitu vielä";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label10.Location = new System.Drawing.Point(411, 125);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(176, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Oltava pienempi kuin tutkittava luku";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 361);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tulosSolovay);
+            this.Controls.Add(this.tulosFermat);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tarkista);
+            this.Controls.Add(this.testaustarkkuus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tutkittavaluku);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Alkulukutesti";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tutkittavaluku;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox testaustarkkuus;
+        private System.Windows.Forms.Button tarkista;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label tulosFermat;
+        private System.Windows.Forms.Label tulosSolovay;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿
+
+/* Algoritmit 2 - kurssin harjoitustyö
+ * Alkulukuanalysointi kahdella eri algoritmilla
+ * Copyright Tero Niemi 2012
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace AlkulukuTesti2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tarkista_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tarkista.Enabled = false;
+               
+
+                // asetetaan biginteger muuttuja p:ksi luku, jota halutaan tutkia
+                // käytetään bigintegeriä, että saadaan tutkittua todella suuria lukuja
+                
+                BigInteger p = BigInteger.Parse(tutkittavaluku.Text);
+                
+               
+
+                // testauskertojen määrä määräytyy käyttäjän tekemän valinnan mukaan
+                int amax = 0;
+                if (testaustarkkuus.SelectedIndex == -1) MessageBox.Show("Valitse testaustarkkuus!");
+                if (testaustarkkuus.SelectedIndex == 0) amax = 3;
+                if (testaustarkkuus.SelectedIndex == 1) amax = 100;
+                if (testaustarkkuus.SelectedIndex == 2) amax = 1000;
+                if (testaustarkkuus.SelectedIndex == 3) amax = 10000;
+
+                // tutkitaan ettei testaustarkkuus ole suurempi kuin tutkittava luku
+                if (p <= amax)
+                {
+                    MessageBox.Show("Tutkittava luku on pienempi tai yhtäsuuri kuin testaustarkkuus!\nPienennä testaustarkkuutta tai vaihda tutkittavaa lukua");
+                }
+                
+                
+                else if (testaustarkkuus.SelectedIndex != -1)
+                {
+      
+
+
+                    // luodaan ajon aikana tarvittavat muuttujat laskentaa varten
+                    // käytetän BigInteger luokan muuttujia, jotta voidaan käsitellä todella suuria lukuja
+                    BigInteger laskentaFermat = new BigInteger();
+                    BigInteger laskentaSolovay = new BigInteger();
+                    BigInteger Solovay_a_p = new BigInteger();
+
+                    // luodaan bool muuttujat molemmille testeille, jotka kertovat onko tutkittava luku
+                    // alkuluku kyseisen testin mukaan. Alustetaan ne trueksi. Kun looppi pyörii, niin
+                    // näitä muuttujia muutetaan vain ja ainoastaan jos havaitaan, että luku ei ole 
+                    // alkuluku. 
+                    bool alkulukuFermat = true;
+                    bool alkulukuSolovay = true;
+
+                    // käynnistetään looppi, joka suorittaa testauksen molemmilla algoritmeilla
+                    // a:ta vaihdetaan niin monta kertaa kuin käyttäjä on valinnut
+                    for (int a = 1; a < amax; a++)
+                    {
+                        // fermatin algoritmi 
+                        // a^p-1 -1 = 1 (mod p)
+                        laskentaFermat = (BigInteger.ModPow(a, (p - 1), p));
+
+                        // jos tulos erisuuri kuin 1, luku ei ole alkuluku
+                        if (laskentaFermat != 1)
+                        {
+                            // ei ole alkuluku, päivitetään tieto muuttujaan
+                            alkulukuFermat = false;
+                        }
+
+                        // Solovay-Strassenin algoritmi 
+                        // (a/p) = a^((p-1)/2) (mod p)
+                        laskentaSolovay = (BigInteger.ModPow(a,((p-1)/2),p));
+
+                        // jos tulos on joko 1 tai p-1 niin tällöin on kyseessä alkuluku
+                        if (laskentaSolovay == 1 || laskentaSolovay == p - 1)
+                        {
+                            // luku on alkuluku, ei tehdä mitään muutoksia
+                        }
+                        else
+                        {
+                            alkulukuSolovay = false;
+                        }
+
+
+                    }
+
+
+                    // tarkistetaan Fermatin algoritmin tulokset
+                    // päivitetään tieto tuloksiin
+                    if (alkulukuFermat)
+                    {
+                        tulosFermat.Text = "Alkuluku";
+                        tulosFermat.ForeColor = Color.Green;
+                    }
+
+                    else
+                    {
+                        tulosFermat.Text = "Ei alkuluku";
+                        tulosFermat.ForeColor = Color.Red;
+                    }
+
+
+                    // tarkistetaan Solovay Strassenin tulokset
+                    // päivitetään tieto tuloksiin
+                    if (alkulukuSolovay)
+                    {
+                        tulosSolovay.Text = "Alkuluku";
+                        tulosSolovay.ForeColor = Color.Green;
+                    }
+
+                    else
+                    {
+                        tulosSolovay.Text = "Ei alkuluku";
+                        tulosSolovay.ForeColor = Color.Red;
+                    }
+
+                    tarkista.Enabled = true;
+                    
+
+                }
+            }
+            catch
+            {
+                // virheidenkäsittely, mikäli luvuksi on syötetty jotain muuta kuin luku (esim. tekstiä)
+                MessageBox.Show("Virhe! Tarkista syöttökentät!");
+                tarkista.Enabled = true;
+                
+            }
+
+        }
+
+       
+
+       
+    }
+}
